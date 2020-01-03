498f28dbef30c56169e654a6a77d4a0be472a9aa
ZhangNing <zhangning885@pingan.com.cn>
2020-01-03 10:04:31 +0800

    init Parking Lot

---
+++ ParkingLot/ParkingLot.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ParkingLot.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyProduct("ParkingLot.Test")]
+[assembly: AssemblyCopyright("Copyright © Microsoft 2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("034b9d79-0971-4b3f-a4ec-77b918c0a398")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ ParkingLot/ParkingLot.Test/UnitTest1.cs
@@ -0,0 +1,13 @@
+﻿using NUnit.Framework;
+
+namespace ParkingLot.Test
+{
+    [TestFixture]
+    public class UnitTest1
+    {
+        [TestCase()]
+        public void WillGetATicketAfterParking()
+        {
+        }
+    }
+}
